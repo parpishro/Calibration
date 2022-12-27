87ba74ae87ddfff772c73c00a04684eb59b3732b
parpishro <par.pishrobat@gmail.com>
2022-12-27 04:20:54 -0800

    editted mu_hat() to be used in update_cov()

--- R/mu_hat.R
+++ R/mu_hat.R
@@ -1,8 +1,9 @@
 # compute the mu_hat based on current (last update) correlation function
 # parameters
 
-mu_hat <- function(y, R) {
-  F <- double(length(y)) + 1
+mu_hat <- function(R, y) {
+
+  f <- double(m) + 1
   invR <- chol2inv(chol(R))
-  return((crossprod(F, invR) %*% y) / chol2inv(chol(crossprod(F, invR) %*% F)))
+  return((crossprod(f, invR) %*% y) / chol2inv(chol(crossprod(f, invR) %*% F)))
 }
