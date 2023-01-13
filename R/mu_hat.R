a3d8459fa5255342f48f93637db9cac832373a89
parpishro <par.pishrobat@gmail.com>
2023-01-12 18:49:41 -0800

    copy pasta!

--- R/mu_hat.R
+++ R/mu_hat.R
@@ -3,7 +3,7 @@
 
 mu_hat <- function(R, y) {
 
-  f <- double(m) + 1
+  f    <- double(m) + 1
   invR <- chol2inv(chol(R))
   return((crossprod(f, invR) %*% y) / chol2inv(chol(crossprod(f, invR) %*% F)))
 }
