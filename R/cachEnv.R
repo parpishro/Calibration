1f6d402b310f5d9070f03a8420e11f37d4574400
parpishro <par.pishrobat@gmail.com>
2023-01-31 00:05:35 -0800

    created checkPriors() to check for invalid user input

--- R/cachEnv.R
+++
@@ -1,4 +0,0 @@
-cacheEnv <- new.env()
-
-
-assign(, , envir = cacheEnv)
---
+++ R/check_priors.R
@@ -0,0 +1,9 @@
+check_priors <- function(t, o, a, s) {
+  if (is.na(t) || is.na(o) || is.na(a) || is.na(s))
+    stop("at least one prior distribution is missing!")
+  if (t == -1 || o == -1 || a == -1 || s == -1)
+    stop("at least one prior distribution is invalid")
+  if (t > 2 || o > 2 || a > 2 || s > 2)
+    stop("at least one prior distribution is still under development")
+  return(TRUE)
+}
