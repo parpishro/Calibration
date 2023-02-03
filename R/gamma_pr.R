dfc1fcfc6ae15d96698e850649b3468db551a9ee
parpishro <par.pishrobat@gmail.com>
2023-02-03 08:45:11 -0800

    deleted the old prior functions after implementing them in the setup_prior function

--- R/gamma_pr.R
+++
@@ -1,6 +0,0 @@
-gamma_pr <- function(gamma, type = "uniform") {
-  if (type=="uniform") {
-    return(0.025)
-  }
-  return(-1)
-}
--- R/sigma2_pr.R
+++
@@ -1,7 +0,0 @@
-sigma2_pr <- function(sigma2, type = "inverse gamma") {
-  if (type=="inverse gamma") {
-    return(0.5 * (exp(sigma2)/((1+exp(sigma2))^2))
-           / sqrt(exp(-sigma2)/(1+exp(-sigma2))))
-  }
-  return(-1)
-}
