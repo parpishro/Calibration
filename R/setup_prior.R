a265ef27394cdf6219c5296cc2ce8db2efafbe0e
Parham <parham@dhcp-206-87-238-217.ubcsecure.wireless.ubc.ca>
2023-02-22 11:43:10 -0800

    completed the prior setup:
    included the prior for scale in Chen's paper
    editted some issues with other prior functions

--- R/setup_prior.R
+++ R/setup_prior.R
@@ -10,55 +10,48 @@
 #'          - fun : density function of chosen prior distribution
 #'          - mean: mean of the chosen distribution (used as initial value)
 setup_prior <- function(pr, p1, p2) {
-  PRIORS  <- c('Uniform', 'Inverse Gamma', 'Gaussian', 'Beta', 'Gamma',
-                'Exponential', 'Logistic', 'Log Beta', 'Jefferys')
+  PRIORS  <- c('uniform', 'inversegamma', 'gaussian', 'beta', 'gamma',
+                'exponential', 'logistic', 'logbeta', 'jefferys', 'chen')
   prior   <- match(pr, PRIORS, nomatch = -1)
-
-  if (is.na(prior))
+  if (is.na(prior)) {
     stop("prior missing!")
-
-  if (prior == -1)
+  } else if (prior == -1) {
     stop("prior invalid!")
-
-  if (prior == 'Uniform') {
+  } else if (prior == 1) {  # uniform
+    # !!! ALL FUNCTIONS RETURN THE LOG VALUE OF THE GIVEN DISTRIBUTION
     fun  <- function(x) dunif(x, min = p1, max =  p2, log = TRUE)
-    mean <- (p1 + p2) / 2
-  }
-
-  if (prior == 'Inverse Gamma') {  # REQUIRE: p1 > 0, x > 0
-    fun  <- function(x) log(p1^p2) - lgamma(x) - ((p1+1) * log(x)) - (p2 / x)
-    mean <- p2 / (p1 - 1)
-  }
-
-  if (prior == 'Gaussian') {
+    m <- (p1 + p2) / 2
+  } else if (prior == 2) { # inversegamma
+    # REQUIRE: p1 > 0, x > 0
+    fun  <- function(x) p1*log(p2) - lgamma(p1) - (p1+1)*log(x) - p2/(x)
+    m <- p2 / (p1 - 1)
+  } else if (prior == 3) { # gaussian
     fun  <- function(x) -log(sqrt(2*p2*pi)) - (((x - p1)^2)/ (2*p2))
-    mean <- p1
-  }
-
-  if (prior == 'Beta') {
+    m <- p1
+  } else if (prior == 4) { # beta
     fun  <- function(x) ((p1-1) * log(x)) + ((p2-1) * (1-x)) - lbeta(p1, p2)
-    mean <- p1 / (p1+p2)
-  }
-
-  if (prior == 'Gamma') {
+    m <- p1 / (p1+p2)
+  } else if (prior == 5) { # gamma
     fun  <- function(x) log(p2^p1) + ((p1-1)*log(x)) - (p2*x) - lgamma(p1)
-    mean <- p1 / p2
-  }
-
-  if (prior == 'Exponential') {
+    m <- p1 / p2
+  } else if (prior == 6) { # exponential
     fun  <- function(x) log(p1) - (p1*x)
-    mean <- 1/p2
-  }
-
-  if (prior == 'Log Beta') {      # TODO
+    m <- 1/p2
+  } else if (prior == 7) { # logistic    # TODO
     fun  <- function(x) 0
-    mean <- 0
-  }
-
-  if (prior == 'Jefferys') {      # TODO
+    m <- 0
+  } else if (prior == 8) { # logbeta     # TODO
     fun  <- function(x) 0
-    mean <- 0
+    m <- 0
+  } else if (prior == 9) { # jefferys   # TODO
+    fun  <- function(x) 0
+    m <- 0
+  } else if (prior == 10) { # chen
+    # from "Flexible Correlation ..." at 10.1137/15M1008774, P606, Eq. 4.1
+    # a log is taken from the equation and simplified
+    fun  <- function(x) log(0.5) + 1.5*x + 0.5*log(1+exp(-x)) - 2*log(1+exp(x))
+    m <- 1 # computed numerically
   }
 
-  return(list(fun = fun, mean = mean))
+  return(list(fun = fun, mean = m))
 }
