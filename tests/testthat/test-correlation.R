540a18eedcdd1c517f1a01aac9720e669b99f71e
parpishro <par.pishrobat@gmail.com>
2023-07-01 15:10:51 -0700

    correlation test.

--- tests/testthat/test-correlation.R
+++ tests/testthat/test-correlation.R
@@ -1,1 +1,9 @@
-test-correlation
+test_that("Error in correlation of a matrix with itself!", {
+  dat <- create_testData()
+  expect_equal(correlation(dat$X, theta = dat$sc, alpha = dat$sm), ExpXX)
+})
+
+test_that("Error in correlation between two different matrices!", {
+  dat <- create_testData()
+  expect_equal(correlation(dat$X, dat$Y, theta = dat$sc, alpha = dat$sm), ExpXY)
+})
