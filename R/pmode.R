5345afa7133574a6f47bf09406d5b05b5f7d4e45
parpishro <par.pishrobat@gmail.com>
2023-07-02 17:09:06 -0700

    fixed a semantic bug in mode estimation.

--- R/pmode.R
+++ R/pmode.R
@@ -1,4 +1,9 @@
-#' Compute Mode of a Vector
+#' Estimate Mode from a Sample
+#'
+#' `pmode` bins sample observations into equally spaced intervals and counts number of
+#' observations in each bin. The bin with highest number of observations represents the
+#' mode and mean of observations in that bin is returned as representative mode of the
+#' vector.
 #'
 #' @param x       numeric vector
 #' @param breaks  integer representing number of bins to compute mode
@@ -7,10 +12,18 @@
 #' @export
 #'
 #' @examples
-pmode <- function(x, breaks = 20) {
-  bins   <- seq(min(x), max(x), length.out = breaks+1)
+pmode <- function(x, breaks = NA) {
+  if (length(x) < 6)
+    return(mean(x))
+  if (is.na(breaks))
+    breaks <- floor(length(x)/5)
+  bounds <- seq(min(x), max(x), length.out = breaks+1)
   counts <- double(breaks)
-  for (i in 2:(breaks+1))
-    counts[i-1] <- sum(x >= bins[i-1] & x < bins[i])
-  return(mean(bins[which.max(counts):(which.max(counts)+1)]))
+  bins   <- list()
+  for (i in 1:breaks) {
+    bins[[i]] <- x[x >= bounds[i] & x < bounds[i + 1]]
+    counts[i] <- length(bins[[i]])
+  }
+  counts[breaks] <- counts[breaks] + 1
+  return(mean(bins[[which.max(counts)]]))
 }
