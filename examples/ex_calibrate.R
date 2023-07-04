a5d8f1972a2e9b8411f025f40388402cc1a34998
parpishro <par.pishrobat@gmail.com>
2023-07-03 18:48:09 -0700

    removed strict integer type requirement

--- examples/ex_calibrate.R
+++ examples/ex_calibrate.R
@@ -3,5 +3,5 @@ cal1    <- calibrate(sim = Ds1, field = Df1,
                      kappa_dist = "gaussian", init = 0.5, p1 = 0.5, p2 = 0.25)
 summary(cal1)
 
-cal2    <- calibrate(sim = Ds2, field = Df2, Nmcmc = 200L, nBurn = 0L, thinning = 1L)
+cal2    <- calibrate(sim = Ds2, field = Df2, Nmcmc = 20L, nBurn = 0L, thinning = 1L)
 summary(cal2)
