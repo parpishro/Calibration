bab148a33958a6196c9d7d87f185a5de157e7fb7
parpishro <par.pishrobat@gmail.com>
2023-07-04 12:53:37 -0700

    made correlation tests self-contained

--- tests/testthat/test-correlation.R
+++ tests/testthat/test-correlation.R
@@ -1,9 +1,30 @@
 test_that("Error in correlation of a matrix with itself!", {
-  dat <- create_testData()
-  expect_equal(correlation(dat$X, theta = dat$sc, alpha = dat$sm), dat$ExpXX)
+  X     <- matrix(c(1, 3, 5,
+                    2, 2, 6,
+                    1, 4, 1), nrow = 3, byrow = TRUE)
+
+  sc    <- c(1, 2, 3)
+  sm    <- c(2, 1, 2)
+
+  ExpXX <- matrix(c(1,        exp(-6),  exp(-50),
+                    exp(-6),  1,        exp(-80),
+                    exp(-50), exp(-80), 1), nrow = 3, byrow = TRUE)
+  expect_equal(correlation(X, theta = sc, alpha = sm), ExpXX)
 })
 
 test_that("Error in correlation between two different matrices!", {
-  dat <- create_testData()
-  expect_equal(correlation(dat$X, dat$Y, theta = dat$sc, alpha = dat$sm), dat$ExpXY)
+    X     <- matrix(c(1, 3, 5,
+                    2, 2, 6,
+                    1, 4, 1), nrow = 3, byrow = TRUE)
+
+  Y     <- matrix(c(7, 3, 0,
+                    2, 2, 4), nrow = 2, byrow = TRUE)
+
+  sc    <- c(1, 2, 3)
+  sm    <- c(2, 1, 2)
+
+  ExpXY <- matrix(c(exp(-111), exp(-6),
+                    exp(-135), exp(-12),
+                    exp(-41),  exp(-32)), nrow = 3, byrow = TRUE)
+  expect_equal(correlation(X, Y, theta = sc, alpha = sm), ExpXY)
 })
