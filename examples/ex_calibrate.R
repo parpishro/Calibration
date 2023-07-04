6290905e9b2e594508d9acc82661048d146ebb63
parpishro <par.pishrobat@gmail.com>
2023-07-03 19:08:32 -0700

    changed to numeric

--- examples/ex_calibrate.R
+++ examples/ex_calibrate.R
@@ -1,7 +1,7 @@
 cal1    <- calibrate(sim = Ds1, field = Df1,
-                     Nmcmc = 5L, nBurn = 0L, thinning = 1L,
-                     kappa_dist = "gaussian", init = 0.5, p1 = 0.5, p2 = 0.25)
+                     Nmcmc = 5, nBurn = 0, thinning = 1,
+                     kappa = list(dist = "gaussian", init = 0.5, p1 = 0.5, p2 = 0.25))
 summary(cal1)
 
-cal2    <- calibrate(sim = Ds2, field = Df2, Nmcmc = 20L, nBurn = 0L, thinning = 1L)
+cal2    <- calibrate(sim = Ds2, field = Df2, Nmcmc = 20, nBurn = 0, thinning = 1)
 summary(cal2)
