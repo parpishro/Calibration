606a17c96cc4127b1220210b08e92d3a5ac83560
parpishro <par.pishrobat@gmail.com>
2023-07-03 17:13:21 -0700

    typo

--- tests/testthat/test-correlation.R
+++ tests/testthat/test-correlation.R
@@ -1,9 +1,9 @@
 test_that("Error in correlation of a matrix with itself!", {
   dat <- create_testData()
-  expect_equal(correlation(dat$X, theta = dat$sc, alpha = dat$sm), ExpXX)
+  expect_equal(correlation(dat$X, theta = dat$sc, alpha = dat$sm), dat$ExpXX)
 })
 
 test_that("Error in correlation between two different matrices!", {
   dat <- create_testData()
-  expect_equal(correlation(dat$X, dat$Y, theta = dat$sc, alpha = dat$sm), ExpXY)
+  expect_equal(correlation(dat$X, dat$Y, theta = dat$sc, alpha = dat$sm), dat$ExpXY)
 })
