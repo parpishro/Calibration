1ad3e53f878f1c2e11c329caf51279d34aec8ebf
parpishro <par.pishrobat@gmail.com>
2023-07-03 17:12:56 -0700

    added second examples (with multiple experimental and calibration inputs)

--- examples/ex_calibrate.R
+++ examples/ex_calibrate.R
@@ -1,4 +1,7 @@
-cal     <- calibrate(sim = Ds, field = Df,
+cal1    <- calibrate(sim = Ds1, field = Df1,
                      Nmcmc = 5L, nBurn = 0L, thinning = 1L,
                      kappa_dist = "gaussian", init = 0.5, p1 = 0.5, p2 = 0.25)
-summary(cal)
+summary(cal1)
+
+cal2    <- calibrate(sim = Ds2, field = Df2, Nmcmc = 200L, nBurn = 0L, thinning = 1L)
+summary(cal2)
