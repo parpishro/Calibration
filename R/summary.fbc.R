a4c259b0418469b2f565f34b3776533dc94e1b98
parpishro <par.pishrobat@gmail.com>
2023-07-10 10:37:06 -0700

    added summary example

--- R/summary.fbc.R
+++ R/summary.fbc.R
@@ -6,9 +6,9 @@
 #'
 #' @return dataframe where rows represent model parameters and columns represent: mean,
 #'         median, mode, lwr50, upr50, lwr80, upr80, sd
-#' @export
 #'
-#' @examples
+#' @example examples/ex_summary.R
+#' @export
 summary.fbc <- function(object) {
   return(object$estimates)
 }
