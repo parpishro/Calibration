2da19441a4c00a164d5df24a37f6ddb87ed5d433
parpishro <par.pishrobat@gmail.com>
2023-08-18 16:52:37 -0700

    fixed the bug in default specification set_hyperPriors()

--- man/examples/ex_set_hyperPriors.R
+++ man/examples/ex_set_hyperPriors.R
@@ -1,6 +1,6 @@
 # set hyperparameter priors by provide argument. All prior setting has reasonable default values
 
-pr1   <- set_hyperPriors(sigma2SDist = "uniform")
+pr1   <- set_hyperPriors(sigma2SDist = "inversegamma")
 cal1  <- calibrate(sim = analytic11S, field = analytic11F,
                    nMCMC = 5, nBurn = 0, thinning = 1,
                    hypers = pr1)
