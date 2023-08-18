303dd9a6f9b8bda2d0862643ea1f1c20e847900c
parpishro <par.pishrobat@gmail.com>
2023-08-18 16:26:21 -0700

    changed set_hyperPriors() interface

--- R/set_hyperPriors.R
+++ R/set_hyperPriors.R
@@ -5,7 +5,6 @@
 #' fields:
 #'  - `dist`: character string (vector of character strings) to specify the family of the
 #'            distributions. All common probability distribution are implemented.
-#'  - `init`: initial value to start the MCMC algorithm
 #'  - `p1`:   first distribution parameter
 #'  - `p2`:   second distribution parameter
 #' All hyperparameters have default values to work with variety of models. Therefore, it
@@ -16,68 +15,51 @@
 #'                       * `p1`:
 #'                       * `p2`:   double (vector of doubles) representing the first parameter(s) of the
 #'                                 chosen distribution(s)
-#'                       * `init`:
 #'
 #' @param thetaSDist     string (vector of strings) to specify the prior distribution type(s) for
 #'                       simulator correlation scale parameters
-#' @param thetaSInit     double (vector of doubles) that represent initial value(s) of simulator
-#'                       correlation scale parameter parameters to start the MCMC
 #' @param thetaSP1       double (vector of doubles) representing the first parameter(s) of the
 #'                       chosen distribution(s) for simulator correlation scale
 #' @param thetaSP2       double (vector of doubles) representing the second parameter(s) of the
 #'                       chosen distribution(s) for simulator correlation scale
 #' @param alphaSDist     string (vector of strings) to specify the prior distribution type(s) for
 #'                       simulator correlation smoothness parameters
-#' @param alphaSInit     double (vector of doubles) that represent initial value(s) of simulator
-#'                       correlation smoothness parameter parameters to start the MCMC
 #' @param alphaSP1       double (vector of doubles) representing the first parameter(s) of the
 #'                       chosen distribution(s) for simulator correlation smoothness
 #' @param alphaSP2       double (vector of doubles) representing the second parameter(s) of the
 #'                       chosen distribution(s) for simulator correlation smoothness
 #' @param thetaBDist     string (vector of strings) to specify the prior distribution type(s) for
 #'                       bias-correction correlation scale parameters
-#' @param thetaBInit     double (vector of doubles) that represent initial value(s) of
-#'                       bias-correction correlation scale parameter parameters to start the MCMC
 #' @param thetaBP1       double (vector of doubles) representing the first parameter(s) of the
 #'                       chosen distribution(s) for bias-correction correlation scale
 #' @param thetaBP2       double (vector of doubles) representing the second parameter(s) of the
 #'                       chosen distribution(s) for bias-correction correlation scale
 #' @param alphaBDist     string (vector of strings) to specify the prior distribution type(s) for
 #'                       bias-correction correlation smoothness parameters
-#' @param alphaBInit     double (vector of doubles) that represent initial value(s) of
-#'                       bias-correction  correlation smoothness parameter parameters to start the MCMC
 #' @param alphaBP1       double (vector of doubles) representing the first parameter(s) of the
 #'                       chosen distribution(s) for bias-correction correlation smoothness
 #' @param alphaBP2       double (vector of doubles) representing the second parameter(s) of the
 #'                       chosen distribution(s) for bias-correction correlation smoothness
 #' @param sigma2SDist    string (vector of strings) to specify the prior distribution type(s) for
 #'                       simulator marginal variance parameter
-#' @param sigma2SInit    double (vector of doubles) that represent initial value(s) of simulator
-#'                       marginal variance parameter parameters to start the MCMC
 #' @param sigma2SP1      double (vector of doubles) representing the first parameter(s) of the
 #'                       chosen distribution(s) for simulator marginal variance
 #' @param sigma2SP2      double (vector of doubles) representing the second parameter(s) of the
 #'                       chosen distribution(s) for simulator marginal variance
 #' @param sigma2BDist    string (vector of strings) to specify the prior distribution type(s) for
 #'                       bias-correction marginal variance parameter
-#' @param sigma2BInit    double (vector of doubles) that represent initial value(s) of
-#'                       bias-correction marginal variance parameter parameters to start the MCMC
 #' @param sigma2BP1      double (vector of doubles) representing the first parameter(s) of the
 #'                       chosen distribution(s) for bias-correction marginal variance
 #' @param sigma2BP2      double (vector of doubles) representing the second parameter(s) of the
 #'                       chosen distribution(s) for bias-correction marginal variance
 #' @param sigma2EDist    string (vector of strings) to specify the prior distribution type(s) for
 #'                       measurement variance  parameter
-#' @param sigma2EInit    double (vector of doubles) that represent initial value(s) of measurement
-#'                       variance parameter parameters to start the MCMC
 #' @param sigma2EP1      double (vector of doubles) representing the first parameter(s) of the
 #'                       chosen distribution(s) for measurement variance
 #' @param sigma2EP2      double (vector of doubles) representing the second parameter(s) of the
 #'                       chosen distribution(s) for measurement variance
 #' @param muBDist        string (vector of strings) to specify the prior distribution type(s) for
 #'                       bias-correction mean parameter
-#' @param muBInit        double (vector of doubles) that represent initial value(s) of
-#'                       bias-correction mean parameter parameters to start the MCMC
 #' @param muBP1          double (vector of doubles) representing the first parameter(s) of the
 #'                       chosen distribution(s) for bias-correction mean
 #' @param muBP2          double (vector of doubles) representing the second parameter(s) of the
@@ -87,54 +69,37 @@
 #'
 #' @example man/examples/ex_set_hyperPriors.R
 #' @export
-set_hyperPriors <- function(thetaSDist  = "gamma",        thetaSInit  = 0.1, thetaSP1  = 1.5, thetaSP2  = 0.5,
-                            alphaSDist  = "betashift",    alphaSInit  = 1.5, alphaSP1  = 5,   alphaSP2  = 2,
-                            thetaBDist  = "gamma",        thetaBInit  = 0.1, thetaBP1  = 1.5, thetaBP2  = 0.5,
-                            alphaBDist  = "betashift",    alphaBInit  = 1.5, alphaBP1  = 5,   alphaBP2  = 2,
-                            sigma2SDist = "inversegamma", sigma2SInit = 1 ,  sigma2SP1 = 100, sigma2SP2 = 0.1,
-                            sigma2BDist = "inversegamma", sigma2BInit = 1,   sigma2BP1 = 1,   sigma2BP2 = 1,
-                            sigma2EDist = "inversegamma", sigma2EInit = 1,   sigma2EP1 = 10,  sigma2EP2 = 1,
-                            muBDist     = "uniform",      muBInit     = 0,   muBP1     = -1,  muBP2     = 1) {
-  stopifnot(sum(!(thetaSDist %in% c("uniform", "gaussian", "gamma", "beta", "lognormal", "logistic",
-                                    "betashift", "exponential", "inversegamma", "jeffreys", "fixed"))) == 0  &&
-              is.double(thetaSInit) && is.double(thetaSP1) && is.double(thetaSP2) &&
-              length(thetaSDist) == length(thetaSP1) &&
-              length(thetaSDist) == length(thetaSP2))
-  stopifnot(sum(!(alphaSDist %in% c("uniform", "gaussian", "gamma", "beta", "lognormal", "logistic",
-                                    "betashift", "exponential", "inversegamma", "jeffreys", "fixed"))) == 0 &&
-              is.double(alphaSInit) && is.double(alphaSP1) && is.double(alphaSP2) &&
-              length(alphaSDist) == length(alphaSP1) &&
-              length(alphaSDist) == length(alphaSP2))
-  stopifnot(sum(!(thetaBDist %in% c("uniform", "gaussian", "gamma", "beta", "lognormal", "logistic",
-                                    "betashift", "exponential", "inversegamma", "jeffreys", "fixed"))) == 0 &&
-              is.double(thetaBInit) && is.double(thetaBP1) && is.double(thetaBP2) &&
-              length(thetaBDist) == length(thetaBP1) &&
-              length(thetaBDist) == length(thetaBP2))
-  stopifnot(sum(!(alphaBDist %in% c("uniform", "gaussian", "gamma", "beta", "lognormal", "logistic",
-                                    "betashift", "exponential", "inversegamma", "jeffreys", "fixed"))) == 0 &&
-              is.double(alphaBInit) && is.double(alphaBP1) && is.double(alphaBP2) &&
-              length(alphaBDist) == length(alphaBP1) &&
-              length(alphaBDist) == length(alphaBP2))
-  stopifnot(sigma2SDist %in% c("uniform", "gaussian", "gamma", "beta", "lognormal", "logistic",
-                                   "betashift", "exponential", "inversegamma", "jeffreys", "fixed")  &&
-              is.double(sigma2SInit) && is.double(sigma2SP1) && is.double(sigma2SP2))
-  stopifnot(sigma2BDist %in% c("uniform", "gaussian", "gamma", "beta", "lognormal", "logistic",
-                                   "betashift", "exponential", "inversegamma", "jeffreys")  &&
-              is.double(sigma2BInit) && is.double(sigma2BP1) && is.double(sigma2BP2))
-  stopifnot(sigma2EDist %in% c("uniform", "gaussian", "gamma", "beta", "lognormal", "logistic",
-                                   "betashift", "exponential", "inversegamma", "jeffreys", "fixed")  &&
-              is.double(sigma2EInit) && is.double(sigma2EP1) && is.double(sigma2EP2))
-  stopifnot(muBDist %in% c("uniform", "gaussian", "gamma", "beta", "lognormal", "logistic",
-                                   "betashift", "exponential", "inversegamma", "jeffreys", "fixed")  &&
-              is.double(muBInit) && is.double(muBP1) && is.double(muBP2))
-  return(list(thetaS  = list(dist = thetaSDist,  init = thetaSInit,  p1 = thetaSP1,  p2 = thetaSP2),
-              alphaS  = list(dist = alphaSDist,  init = alphaSInit,  p1 = alphaSP1,  p2 = alphaSP2),
-              thetaB  = list(dist = thetaBDist,  init = thetaBInit,  p1 = thetaBP1,  p2 = thetaBP2),
-              alphaB  = list(dist = alphaBDist,  init = alphaBInit,  p1 = alphaBP1,  p2 = alphaBP2),
-              sigma2S = list(dist = sigma2SDist, init = sigma2SInit, p1 = sigma2SP1, p2 = sigma2SP2),
-              sigma2B = list(dist = sigma2BDist, init = sigma2BInit, p1 = sigma2BP1, p2 = sigma2BP2),
-              sigma2E = list(dist = sigma2EDist, init = sigma2EInit, p1 = sigma2EP1, p2 = sigma2EP2),
-              muB     = list(dist = muBDist,     init = muBInit,     p1 = muBP1,     p2 = muBP2)))
+set_hyperPriors <- function(thetaSDist  = "logbeta",      thetaSP1  = 5, thetaSP2  = 5,
+                            alphaSDist  = "betashift",    alphaSP1  = 5,   alphaSP2  = 2,
+                            thetaBDist  = "logbeta",      thetaBP1  = 5, thetaBP2  = 5,
+                            alphaBDist  = "betashift",    alphaBP1  = 5,   alphaBP2  = 2,
+                            sigma2SDist = "inversegamma", sigma2SP1 = 5,   sigma2SP2 = 5,
+                            sigma2BDist = "inversegamma", sigma2BP1 = 12,  sigma2BP2 = 2,
+                            sigma2EDist = "inversegamma", sigma2EP1 = 1,   sigma2EP2 = 0.01,
+                            muBDist     = "uniform",      muBP1     = -1,  muBP2     = 1) {
+  dists <- c(thetaSDist, alphaSDist, thetaBDist, alphaBDist, sigma2SDist, sigma2BDist, sigma2EDist, muBDist)
+  p1s   <- c(thetaSP1, alphaSP1, thetaBP1, alphaBP1, sigma2SP1, sigma2BP1, sigma2EP1, muBP1)
+  p2s   <- c(thetaSP2, alphaSP2, thetaBP2, alphaBP2, sigma2SP2, sigma2BP2, sigma2EP2, muBP2)
+
+  if (!is.character(dists))
+    stop("'dist' argument must be character string!")
+  if (sum(!(dists %in% c("uniform", "normal", "normalTr", "lognormal", "gamma", "inversegamma",
+                         "beta", "betashift", "logbeta", "logistic","exponential", "fixed"))) > 0)
+    stop("'dist' argument must be one of 'uniform', 'normal', 'normalTr', 'lognormal', 'gamma',
+          'inversegamma', 'beta', 'betashift', 'logbeta', 'logistic','exponential', 'fixed'")
+  if (sum(!is.double(p1s)) + sum(!is.double(p2s)) > 0)
+      stop("Both 'p1' and `p2` arguments must be double!")
+  if (length(dists) != length(p1s) || length(dists) != length(p2s))
+      stop("Length of 'dist', 'p1', and 'p2' arguments must be same!")
+
+  return(list(thetaS  = list(dist = thetaSDist,  p1 = thetaSP1,  p2 = thetaSP2),
+              alphaS  = list(dist = alphaSDist,  p1 = alphaSP1,  p2 = alphaSP2),
+              thetaB  = list(dist = thetaBDist,  p1 = thetaBP1,  p2 = thetaBP2),
+              alphaB  = list(dist = alphaBDist,  p1 = alphaBP1,  p2 = alphaBP2),
+              sigma2S = list(dist = sigma2SDist, p1 = sigma2SP1, p2 = sigma2SP2),
+              sigma2B = list(dist = sigma2BDist, p1 = sigma2BP1, p2 = sigma2BP2),
+              sigma2E = list(dist = sigma2EDist, p1 = sigma2EP1, p2 = sigma2EP2),
+              muB     = list(dist = muBDist,     p1 = muBP1,     p2 = muBP2)))
 }
 
 
--- man/examples/ex_set_hyperPriors.R
+++ man/examples/ex_set_hyperPriors.R
@@ -1,12 +1,12 @@
 # set hyperparameter priors by provide argument. All prior setting has reasonable default values
 
-pr1   <- set_hyperPriors(sigma2SDist = "jeffreys")
-cal1  <- calibrate(sim = Ds1, field = Df1,
+pr1   <- set_hyperPriors(sigma2SDist = "uniform")
+cal1  <- calibrate(sim = analytic11S, field = analytic11F,
                    nMCMC = 5, nBurn = 0, thinning = 1,
                    hypers = pr1)
 pr2   <- set_hyperPriors(alphaSDist = c("gamma", "beta"),
                          alphaSP1 = c(1, 2),
                          alphaSP2 = c(2, 4))
-cal2  <- calibrate(sim = Ds1, field = Df1,
+cal2  <- calibrate(sim = analytic11S, field = analytic11F,
                    nMCMC = 5, nBurn = 0, thinning = 1,
                    hypers = pr2)
